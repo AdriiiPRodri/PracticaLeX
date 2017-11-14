d8bd0b99673bbd6725fe624c11c6673542ec48fc
Adrián Jesús Peña Rodríguez <adrian.j.p.r@gmail.com>
2017-11-14 13:04:45 +0100

    LocalesVSGlobales

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -4,16 +4,19 @@
 
 
 
-                              #ifdef _WIN32
+                              #ifdef                  _WIN32
 #include <windows.h>
 #endif
-#include "stdio.h"
+#include            "stdio.h"
 #include "stdlib.h"
 #include "fstream"
 
 
-int main(){    int       a;
-                                int b;
+int Valor,valor2,valor3;
+int valor1;
+
+int main(){int       A;
+int b;
 
     printf("Prueba\n");
     return 0;
